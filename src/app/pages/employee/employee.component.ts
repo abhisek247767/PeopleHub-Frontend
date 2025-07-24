93b9aada85ad07098155d730eaed0da4703a3654
AbhisekRoy-1 <abhisekroy169@gmail.com>
2025-07-24 23:20:11 +0530

    feat: enhance project management with new project form and employee loading functionality

--- src/app/pages/employee/employee.component.ts
+++ src/app/pages/employee/employee.component.ts
@@ -1,43 +1,44 @@
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
 import { Select2Module } from 'ng-select2-component';
+
 import { IApiResponse, IChildDept, IParentDept } from '../../model/interface/master';
 import { MasterService } from './../../service/master.service';
-import { Component, inject, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
-
-
+import { EmployeeService } from '../../employee.service';
 @Component({
   selector: 'app-employee',
-  imports: [FormsModule,Select2Module,CommonModule],
+  imports: [ Select2Module, CommonModule],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
 export class EmployeeComponent implements OnInit {
-   
 
   isFormVisiable: boolean = false;
-
-  masterService = inject(MasterService)
-  parentDeptList: IParentDept[] = []
-  childDeptListById: IChildDept[] = []
+  parentDeptList: IParentDept[] = [];
+  childDeptListById: IChildDept[] = [];
+  employeeForm!: FormGroup;
   parentDeptId: number = 0;
 
- 
-  
+  masterService = inject(MasterService);
+  employeeService = inject(EmployeeService);
+  fb = inject(FormBuilder);
 
+  ngOnInit(): void {
+    this.employeeForm = this.fb.group({
+      employeeName: ['', Validators.required],
+      contactNo: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      gender: ['', Validators.required],
+      parentDeptId: ['', Validators.required],
+      childDeptId: ['', Validators.required],
+      password: ['', Validators.required],
+      role: ['', Validators.required],
+    });
 
-  getParentDept() {
-    this.masterService.getAllDept().subscribe(
-      (res: IApiResponse) => {
-        this.parentDeptList = res.data;  
-      },
-      (error) => {
-        console.error('Error fetching departments:', error);
-      }
-    );
+    this.isFormVisiable = false;
   }
+  
 
   onParentDeptChange(){
     this.masterService.getAllChildDeptBy(this.parentDeptId).subscribe(
@@ -54,14 +55,21 @@ export class EmployeeComponent implements OnInit {
   }
 
   closeForm() {
-    console.log("close clicked");
     this.isFormVisiable = false;
-
- 
-
   }
-    ngOnInit(): void {
-    // You can optionally reset here too if needed
-    this.isFormVisiable = false;
+  onSubmit(): void {
+
+
+    const payload = this.employeeForm.value;
+
+    this.employeeService.addEmployee(payload).subscribe({
+      next: (res) => {
+        console.log('Employee Added', res);
+        // reset form or show success message
+      },
+      error: (err) => {
+        console.error('Error adding employee', err);
+      },
+    });
 }
 }
